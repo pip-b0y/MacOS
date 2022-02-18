3bbf485b63c267f0af2cc0ecc1f22b726db62e01
Hayden Charter <69624918+pip-b0y@users.noreply.github.com>
2022-02-18 13:00:34 +1100

    Update Allow_standard_user_to_Change_network_settings.sh
    comments in script

--- Granular_settings/Allow_standard_user_to_Change_network_settings.sh
+++ Granular_settings/Allow_standard_user_to_Change_network_settings.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Script is as is
+#Tested on a M1 device running latest version of MacOS12.2
+#Note that settings may change in future releases of MacOS. Must run as root in a policy
 /usr/bin/security authorizationdb write system.preferences.network allow
 /usr/bin/security authorizationdb write system.services.systemconfiguration.network allow
 exit 0
